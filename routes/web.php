a6e002a3dfd35fd7b35d09f9c9afb6fe0e757d22
uluumbch <uluum123@gmail.com>
2025-09-25 08:44:49 +0800

    mmebuat page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', [LandingPageController::class, 'home']);
 
 Route::get('/kendaraan', [LandingPageController::class, 'kendaraan']);
+
+Route::get('/login', [AuthController::class, 'loginPage']);
