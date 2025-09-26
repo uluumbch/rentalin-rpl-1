8555ea1cb097ceb5a85245fadaea74e1e1e1962b
uluumbch <uluum123@gmail.com>
2025-09-26 15:06:57 +0800

    CRUD

--- app/Http/Controllers/KendaraanController.php
+++ app/Http/Controllers/KendaraanController.php
@@ -2,12 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kendaraan;
 use Illuminate\Http\Request;
 
 class KendaraanController extends Controller
 {
     public function index()
     {
-        return view('admin.kendaraan');
+        $kendaraan = Kendaraan::all();
+
+        return view('admin.kendaraan', [
+            'kendaraan' => $kendaraan,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.tambah_kendaraan');
+    }
+
+    public function store(Request $request)
+    {
+        $nama = $request->input('nama');
+        $harga = $request->input('harga');
+
+        $gambar = $request->file('gambar')->store('kendaraan', 'public');
+
+        Kendaraan::create([
+            'nama' => $nama,
+            'harga' => $harga,
+            'gambar' => $gambar,
+        ]);
+
+        return redirect()->route('admin.kendaraan.index');
+    }
+
+    public function edit($id)
+    {
+        $kendaraan = Kendaraan::findOrFail($id);
+
+        return view('admin.edit_kendaraan', [
+            'kendaraan' => $kendaraan,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $kendaraan = Kendaraan::findOrFail($id);
+
+        $nama = $request->input('nama');
+        $harga = $request->input('harga');
+
+        // Handle file upload jika ada
+        if ($request->hasFile('gambar')) {
+            $imagePath = $request->file('gambar')->store('kendaraan', 'public');
+            $kendaraan->gambar = $imagePath;
+        }
+
+        // Update data kendaraan
+        $kendaraan->nama = $nama;
+        $kendaraan->harga = $harga;
+        $kendaraan->save();
+
+        return redirect()->route('admin.kendaraan.index')->with('success', 'Kendaraan berhasil diperbarui.');
+    }
+
+    public function destroy($id)
+    {
+        $kendaraan = Kendaraan::findOrFail($id);
+
+        unlink(public_path('storage/'.$kendaraan->gambar));
+
+        $kendaraan->delete();
+
+        return redirect()->route('admin.kendaraan.index')->with('success', 'Kendaraan berhasil dihapus.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\KendaraanController;
+use App\Http\Controllers\LandingPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingPageController::class, 'home']);
@@ -23,3 +23,23 @@ Route::get('/admin', [AdminController::class, 'admin'])
 Route::get('/admin/kendaraan', [KendaraanController::class, 'index'])
     ->middleware('auth')
     ->name('admin.kendaraan.index');
+
+Route::get('/admin/kendaraan/tambah', [KendaraanController::class, 'create'])
+    ->middleware('auth')
+    ->name('admin.kendaraan.create');
+
+Route::post('/admin/kendaraan', [KendaraanController::class, 'store'])
+    ->middleware('auth')
+    ->name('admin.kendaraan.store');
+
+Route::get('/admin/kendaraan/{id}/edit', [KendaraanController::class, 'edit'])
+    ->middleware('auth')
+    ->name('admin.kendaraan.edit');
+
+Route::put('/admin/kendaraan/{id}', [KendaraanController::class, 'update'])
+    ->middleware('auth')
+    ->name('admin.kendaraan.update');
+
+Route::delete('/admin/kendaraan/{id}', [KendaraanController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('admin.kendaraan.destroy');
